238ef9bc1ecddee6000079a5399378d71fa6d7dd
Luka Weis <lukat.weis@gmail.com>
2022-03-02 17:51:12 +0100

    initial commit

---
+++ example.ps1
@@ -0,0 +1,14 @@
+﻿configuration Software
+{
+    Import-DscResource -ModuleName PsDesiredStateConfiguration
+    Import-DscResource -Module xNodeJS
+
+    node ("localhost")
+    {
+        xNodeJS NodeJSLatestStable {
+            Ensure       = 'Present'
+            MajorVersion = 'LatestStable'
+            #MajorVersion = '16.13.0'
+        }
+    }
+}
